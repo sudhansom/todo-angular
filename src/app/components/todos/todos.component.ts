65ed5077d06063a6d419f2401d647a7aee04618f
bhuwan <bkspoudel@gmail.com>
2022-06-04 10:32:11 +0200

    displayed todos

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Todo } from './../../models/Todos';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css'],
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[] = [
+    {
+      content: 'first todo',
+      completed: false,
+    },
+    {
+      content: 'second todo',
+      completed: false,
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
